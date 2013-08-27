be02b20919cb4d7e9a780a4b4ed99e1b63e5ea69
TheReverend403 <TheReverend403@cookieslap.net>
2013-08-27 16:22:12 +0100

    Update screenbash.sh

--- screenbash.sh
+++ screenbash.sh
@@ -2,7 +2,7 @@
 # Generate a random file name
 FILE="$(date +%s | sha256sum | base64 | head -c 5)"
 # Prompt you to select a region
-notify-send Screenshot "Select a screenshot region." -t 1000
+notify-send Screenbash "Select a screenshot region." -t 1000
 # Take the screenshot. Click + drag to select the region.
 scrot -s $HOME/Pictures/Screenshots/$FILE.png
 # Make sure the file exists. This allows you to cancel a screenshot.
@@ -13,5 +13,5 @@ then
     # Copy the link to your clipboard
     echo "http://your.domain.com/$FILE" | xsel -i -b
     # Tell you the upload is complete
-    notify-send Screenshot "http://your.domain.com/$FILE copied to clipboard." -i $HOME/Pictures/Screenshots/$FILE.png -t 1000
+    notify-send Screenbash "http://your.domain.com/$FILE copied to clipboard." -i $HOME/Pictures/Screenshots/$FILE.png -t 1000
 fi
