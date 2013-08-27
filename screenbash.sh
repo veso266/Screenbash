0b082f193387f1431527e7f291644194f3d68602
TheReverend403 <TheReverend403@cookieslap.net>
2013-08-27 16:34:37 +0100

    muh grammar

--- screenbash.sh
+++ screenbash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Generate a random file name
+# Generate a random file name.
 FILE="$(date +%s | sha256sum | base64 | head -c 5)"
-# Prompt you to select a region
+# Prompt you to select a region.
 notify-send Screenbash "Select a screenshot region." -t 1000
 # Take the screenshot. Click + drag to select the region.
 scrot -s $HOME/Pictures/Screenshots/$FILE.png
@@ -9,9 +9,9 @@ scrot -s $HOME/Pictures/Screenshots/$FILE.png
 if [ -f $HOME/Pictures/Screenshots/$FILE.png ]
 then
     # Upload the file to a web folder on your server. This is the part where RSA keys are needed.
-    rsync $HOME/Pictures/Screenshots/$FILE.png user@server.com:/directory/for/uploads/$FILE.png
-    # Copy the link to your clipboard
-    echo "http://your.domain.com/$FILE" | xsel -i -b
-    # Tell you the upload is complete
-    notify-send Screenbash "http://your.domain.com/$FILE copied to clipboard." -i $HOME/Pictures/Screenshots/$FILE.png -t 1000
+    rsync $HOME/Pictures/Screenshots/$FILE.png rev@revthefox.co.uk:~/httpdocs/img/$FILE.png
+    # Copy the link to your clipboard.
+    echo "http://i.revthefox.co.uk/$FILE" | xsel -i -b
+    # Tell you the upload is complete.
+    notify-send Screenbash "http://i.revthefox.co.uk/$FILE copied to clipboard." -i $HOME/Pictures/Screenshots/$FILE.png -t 1000
 fi
