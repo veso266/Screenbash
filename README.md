a132740fb9dbec4c06a0a034bb351d42136560d8
Lee Watson <rev@revthefox.co.uk>
2014-09-01 18:58:59 +0100

    Now supports files and screenshots in once script

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 Screenbash
 ==========
 
-Screenshot uploader for Bash.
+Screenshot/file uploader for Bash.
 
-@md_5 WISHES he could be on my level.
+Takes screenshots and uploads them to your own server, and copies the link to your clipboard.
+
+Works with files other than images with the Zenity tool.
+
+Requirements
+============
+
+Zenity
+
+A screenshot tool (gnome-screenshot, scrot, etc...)
+
+A web server running PHP.
+
+Installation
+============
+
+Upload "upload.php" to a web server of your choice and edit the config values, especially the access key.
+
+Place screenbash.sh in a folder on your local system. Edit the config values inside it to match upload.php, and change the save directory and screenshot program if you wish.
+
+Usage
+=====
+
+    screenbash.sh screenshot ## Prompts for a screenshot and then uploads it.
+
+    screenbash.sh file ## Prompts you to select a file and then uploads it.
--- screenbash.sh
+++ screenbash.sh
@@ -1,30 +1,70 @@
 #!/bin/bash
 
-## VARS
+## Configuration - modify these
 # Your upload location. No trailing /
 URL="http://example.com"
 # Name of the PHP script on the remote end
 SCRIPT="upload.php"
 # Key for the PHP script. See upload.php
 KEY="secret"
-# File extension for the image
-EXT="png"
-# Path to save files to locally. No trailing /
-LOCALPATH="$HOME/uploads/screenshots"
 
-# Generate a random file name
-FILE="$(date +%s | sha256sum | head -c 9).$EXT"
-# Prompt you to select a region
-notify-send Screenbash "Select a screenshot region." -t 2000
-# Take the screenshot. Click + drag to select the region.
-gnome-screenshot -a -f $LOCALPATH/$FILE
-#scrot -s $HOME/uploads/screenshots/$FILE.png
-# Make sure the file exists. This allows you to cancel a screenshot.
-if [ -f $LOCALPATH/$FILE ]
-then
-    FINAL=$(curl -F "file=@$LOCALPATH/$FILE" -F "key=$KEY" "$URL/$SCRIPT")
-    # Copy the link to your clipboard
-    echo $FINAL | xsel -i -b
-    # Tell you the upload is complete
-    notify-send Screenbash "$FINAL copied to clipboard." -i $LOCALPATH/$FILE -t 2000
-fi
+## The following only affects screenshots
+# File extension for images
+EXT="png"
+# The command to use for screenshots. The filename will be appended to this.
+# ex. "scrot -s"
+SCREENSHOT="gnome-screenshot -a -f"
+# Path to save screenshots to locally. No trailing /
+# Saves to /tmp by default, which is cleared on reboot.
+# Change this to keep a local copy of the files you upload.
+LOCALPATH="/tmp"
+## Configuration end
+
+# Takes a screenshot, then uploads it.
+screenshot() {
+    # Generate a random file name.
+    FILE="$LOCALPATH/$(date +%s | sha256sum | head -c 9).$EXT"
+    # Prompt you to select a region.
+    notify-send Screenbash "Select a screenshot region." -t 2000
+    # Take the screenshot. Click + drag to select the region.
+    echo "$FILE"
+    $SCREENSHOT "$FILE"
+    upload_file 
+}
+
+# Uploads other files. Please make sure the file you are uploading is allowed in upload.php
+file() {
+    # Prompt the user with a Zenity file selection.
+    FILE=$(zenity --file-selection --title="Select a file")
+    echo "$FILE"
+    upload_file
+}
+
+upload_file() {
+    if [ -f $FILE ]
+    then
+        FINAL=$(curl -F "file=@$FILE" -F "key=$KEY" "$URL/$SCRIPT")
+        # Copy the link to your clipboard
+        echo $FINAL | xsel -i -b
+        # Tell you the upload is complete
+        notify-send Screenbash "$FINAL copied to clipboard." -i "$FILE" -t 2000
+    fi
+}
+
+usage() {
+    echo "Usage:"
+    echo "'screenbash.sh screenshot' - Takes and uploads a screenshot"
+    echo "'screenbash.sh file' - Uploads a file - Requires Zenity"
+}
+
+case $1 in
+    screenshot)
+        screenshot
+        ;;
+    file)
+        file
+        ;;
+    *)
+        usage
+        ;;
+esac
