3a148affab25dd6fb8a1a71c8365b890322eca84
Lee Watson <rev@revthefox.co.uk>
2014-09-02 20:23:14 +0100

    Cleanup KDE code, only generate a random file once, remove debug code

--- screenbash.sh
+++ screenbash.sh
@@ -13,52 +13,70 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-## Configuration - modify these
+##################################
+## Configuration - modify these ##
+##################################
+
 # Your upload location. No trailing /
 URL="http://example.com"
+
 # Name of the PHP script on the remote end
 SCRIPT="upload.php"
+
 # Key for the PHP script. See upload.php
 KEY="secret"
+
 # Length of file names from generated URLs
 URLLENGTH=5
 
 ## The following only affects screenshots
 # File extension for images
 EXT="png"
+
 # The command to use for screenshots. The filename will be appended to this.
 # ex. "scrot -s"
-if (which gnome-screenshot &>/dev/null); then
-    SCREENSHOT="gnome-screenshot -a -f"
-    # Generate a random file name.
-    FILE="$LOCALPATH/$(date +%s | sha256sum | head -c 9).$EXT"
-elif (which kbackgroundsnapshot &>/dev/null); then
-    SCREENSHOT="kbackgroundsnapshot --region"
-    FILE="kde"
-else
-    echo "Can't find a suitable screenshot application."
-    echo "Please install gnome-screenshot or ksnapshot."
-    exit 1
-fi
+# Leave empty to autodetect.
+SCREENSHOT=""
 
 # Path to save screenshots to locally. No trailing /
 # Saves to /tmp by default, which is cleared on reboot.
 # Change this to keep a local copy of the files you upload.
 LOCALPATH="/tmp"
-## Configuration end
+
+#######################
+## Configuration end ##
+#######################
+
+# Autodetect screenshot program.
+if [ $SCREENSHOT == "" ]; then
+    if (which gnome-screenshot &>/dev/null); then
+        SCREENSHOT="gnome-screenshot -a -f"
+    elif (which kbackgroundsnapshot &>/dev/null); then
+        SCREENSHOT="kbackgroundsnapshot --region"
+        KDE_SCREENSHOT=true
+    elif (which scrot &>/dev/null); then
+        SCREENSHOT="scrot -s"
+    else
+        echo "Can't find a suitable screenshot application."
+        echo "Please install gnome-screenshot, ksnapshot or scrot."
+        exit 1
+    fi
+fi
 
 # Takes a screenshot, then uploads it.
 screenshot() {
     # Prompt you to select a region.
     notify-send Screenbash "Select a screenshot region." -t 2000
 
-    if [[ "$FILE" == "kde" ]]; then
+    if $KDE_SCREENSHOT; then
         # KDE needs to take the screenshot first, and then it gives you a name.
         if $SCREENSHOT; then
             # Grab the filename from the Desktop dir.
             FILE="$(ls --sort=time ~/Desktop/snapshot*.png | head -1)"
         fi
     else
+        # Generate a random filename.
+        FILE="$LOCALPATH/$(date +%s | sha256sum | head -c 9).$EXT"
         # Take the screenshot. Click + drag to select the region.
         $SCREENSHOT "$FILE"
     fi
@@ -76,18 +94,12 @@ file() {
 upload_file() {
     if [ -n "$FILE" ]; then
         if [ -f "$FILE" ]; then
-            echo "Created: $FILE"
-            return 0
             FINAL=$(curl -F "file=@$FILE" -F "key=$KEY" -F "length=$URLLENGTH" "$URL/$SCRIPT")
             # Copy the link to your clipboard
             echo $FINAL | xsel -i -b
             # Tell you the upload is complete
             notify-send Screenbash "$FINAL copied to clipboard." -i "$FILE" -t 2000
-        else
-            echo "Unable to create the file: $FILE"
         fi
-    else
-        echo "Unable to determine a file name for: ${SCREENSHOT}"
     fi
 }
 
